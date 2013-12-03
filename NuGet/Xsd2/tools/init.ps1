bbad18c50a5720b6b2c7099ae5915994d1abca2e
Marko Stijak <mstijak@gmail.com>
2013-12-03 07:35:07 +0100

    Update init.ps1

--- NuGet/Xsd2/tools/init.ps1
+++ NuGet/Xsd2/tools/init.ps1
@@ -1,3 +1,3 @@
 param($installPath, $toolsPath, $package, $project)
 mkdir $toolsPath\..\..\Tools
-copy $toolsPath\xsd2.exe $toolsPath\..\..\Tools\
+copy $toolsPath\xsd2.exe $toolsPath\..\..\..\Tools\
