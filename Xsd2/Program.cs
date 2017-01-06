d0071aa680133c561746dd8df468cd2b7eb1821c
Mark Junker <m.junker@fubar-dev.de>
2017-01-06 10:58:15 +0100

    Use Mono.Options and show help if no input files were given - avoide having a separate assembly by using Fody and Costura

--- Xsd2/Program.cs
+++ Xsd2/Program.cs
@@ -1,25 +1,15 @@
 ﻿using System;
-using System.CodeDom;
-using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Schema;
-using System.Xml.Serialization;
-using Microsoft.CSharp;
 
 namespace Xsd2
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             try
             {
-                var inputs = new List<String>();
-
                 var options = new XsdCodeGeneratorOptions
                 {
                     UseNullableTypes = false,
@@ -33,102 +23,38 @@ namespace Xsd2
                     UsingNamespaces = new List<string>()
                 };
 
-                var generator = new XsdCodeGenerator() { Options = options };
                 String outputDirectory = null;
-
                 var combine = false;
+                var help = false;
 
-                foreach (var arg in args)
+                var optionSet = new Mono.Options.OptionSet()
                 {
-                    if (!arg.StartsWith("/"))
-                        inputs.Add(arg);
-                    else
-                    {
-                        String option, value;
-                        var colonIndex = arg.IndexOf(':');
-                        if (colonIndex == -1)
-                        {
-                            option = arg;
-                            value = null;
-                        }
-                        else
-                        {
-                            option = arg.Substring(0, colonIndex + 1);
-                            value = arg.Substring(colonIndex + 1);
-                        }
+                    { "?|h|help", "Shows the help text", s => help = true },
+                    { "o|output=", "Sets the output directory", s => outputDirectory = s },
+                    { "lists", "Use lists", s => options.UseLists = s != null },
+                    { "strip-debug-attributes", "Strip debug attributes", s => options.StripDebuggerStepThroughAttribute = s != null },
+                    { "capitalize", "Capitalize properties", s => options.CapitalizeProperties = s != null },
+                    { "capitalize-enum-values", "Capitalize enum values", s => options.CapitalizeEnumValues = s != null },
+                    { "mixed", "Support mixed content", s => options.MixedContent = s != null },
+                    { "n|ns|namespace=", "Sets the output namespace", s => options.OutputNamespace = s },
+                    { "import=", "Adds import", s => options.Imports.Add(s) },
+                    { "u|using=", "Adds a namespace to use", s => options.UsingNamespaces.Add(s) },
+                    { "ei|exclude-imports", "Exclude imported types", s => options.ExcludeImportedTypes = s != null },
+                    { "ein|exclude-imports-by-name", "Exclude imported types by name", s => options.ExcludeImportedTypes = options.ExcludeImportedTypesByNameAndNamespace = s != null },
+                    { "nullable", "Use nullable types", s => options.UseNullableTypes = s != null },
+                    { "all", "Enable all flags", s => options.CapitalizeProperties = options.StripDebuggerStepThroughAttribute = options.UseLists = options.UseNullableTypes = options.ExcludeImportedTypes = options.MixedContent = s != null },
+                    { "combine", "Combine output to a single file", s => combine = s != null }
+                };
 
-                        switch (option.ToLower())
-                        {
-                            case "/o:":
-                            case "/output:":
-                                outputDirectory = value;
-                                break;
-
-                            case "/lists":
-                                options.UseLists = true;
-                                break;
-
-                            case "/strip-debug-attributes":
-                                options.StripDebuggerStepThroughAttribute = true;
-                                break;
-
-                            case "/capitalize":
-                                options.CapitalizeProperties = true;
-                                break;
-
-                            case "/capitalize-enum-values":
-                                options.CapitalizeEnumValues = true;
-                                break;
-
-                            case "/mixed":
-                                options.MixedContent = true;
-                                break;
-
-                            case "/n:":
-                            case "/ns:":
-                            case "/namespace:":
-                                options.OutputNamespace = value;
-                                break;
-
-                            case "/import:":
-                                options.Imports.Add(value);
-                                break;
-
-                            case "/u:":
-                            case "/using:":
-                                options.UsingNamespaces.Add(value);
-                                break;
-
-                            case "/ei":
-                            case "/exclude-imports":
-                                options.ExcludeImportedTypes = true;
-                                break;
-
-                            case "/ein":
-                            case "/exclude-imports-by-name":
-                                options.ExcludeImportedTypes = true;
-                                options.ExcludeImportedTypesByNameAndNamespace = true;
-                                break;
-
-                            case "/nullable":
-                                options.UseNullableTypes = true;
-                                break;
-
-                            case "/all":
-                                options.CapitalizeProperties = true;
-                                options.StripDebuggerStepThroughAttribute = true;
-                                options.UseLists = true;
-                                options.UseNullableTypes = true;
-                                options.ExcludeImportedTypes = true;
-                                options.MixedContent = true;
-                                break;
-
-                            case "/combine":
-                                combine = true;
-                                break;
-                        }
-                    }
+                var inputs = optionSet.Parse(args);
+                if (help || args.Length == 0 || inputs.Count == 0)
+                {
+                    Console.Error.WriteLine("Xsd2 [options] schema.xsd ...");
+                    optionSet.WriteOptionDescriptions(Console.Error);
+                    return 1;
                 }
+
+                var generator = new XsdCodeGenerator() { Options = options };
 
                 if (combine)
                 {
@@ -167,7 +93,10 @@ namespace Xsd2
             {
                 Console.Error.WriteLine("XSD2 code generation failed.");
                 Console.Error.Write(ex.ToString());
+                return 2;
             }
+
+            return 0;
         }
     }
 }
